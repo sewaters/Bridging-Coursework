708e94f2c94b6986c427be5de947fe88b4025df5
sxw854 <sxw854@cs.bham.ac.uk>
2020-06-22 11:21:17 +0100

    Redesign and nav bar

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+def home_page(request):
+    return render(request, 'blog/home_page.html')
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
