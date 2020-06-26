efbdb7f8180af3f819f3271eb4b60c539a0aacf0
sxw854 <sxw854@cs.bham.ac.uk>
2020-06-26 22:20:37 +0100

    Refactor home pageview to use a template

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.urls import resolve
 from django.test import TestCase
 from blog.views import home_page
@@ -7,3 +8,15 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.strip().startswith('<html>'))
+        self.assertIn('<title>Sara Waters</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'blog/home_page.html')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
