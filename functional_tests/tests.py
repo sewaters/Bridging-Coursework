8bc1d41d6e12a6b1cd972c52cfb1227e5675f053
sxw854 <sxw854@cs.bham.ac.uk>
2020-07-07 16:42:13 +0100

    Rearranged tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -14,7 +15,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool personal website. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000/CV')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('Sara', self.browser.title)
